b001809c4e339411a6d2f0ccffb0c5ab4116a655
DonChrapek <daniel.chrapczynski@gmail.com>
2017-10-31 16:29:49 +0100

    Made ID generator and constructor

--- studentapp/Student.java
+++ studentapp/Student.java
@@ -1,5 +1,48 @@
 package studentapp;
 
-public class Student {
+import java.util.Scanner;
 
+public class Student {
+	private String firstName;
+	private String lastName;
+	private int year;
+	private String studentID;
+	private String courses;
+	private int tuitionBalance;
+	private static int costOfCourse = 600;
+	private static int id = 1000;
+
+	// Constructor: prompt user to enter name and year
+	public Student() {
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Enter student first name: ");
+		this.firstName = in.nextLine();
+		
+		System.out.println("Enter student last name: ");
+		this.lastName = in.nextLine();  
+		
+		System.out.println("Enter student class level (1-4): ");
+		this.year = Integer.parseInt(in.nextLine());
+		
+		setStudentID();
+		
+		System.out.println(firstName + " " + lastName + " " + year + ", ID: " + studentID);
+	}
+	
+	// Generate ID
+	private void setStudentID() {
+		id++;
+		this.studentID =  year + "" + id;
+	}
+	
+	// Enroll in courses
+	
+	// View balance
+	
+	// Pay Tuition
+	
+	public String getName() {
+		return "Name: " + this.firstName + " " + this.lastName;
+	}
 }
--- studentapp/StudentApp.java
+++ studentapp/StudentApp.java
@@ -2,10 +2,10 @@ package studentapp;
 
 public class StudentApp {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {	
+		Student stu1 = new Student();
 		// Ask how many students to add
-
-		
+				
 		// Add n students
 	}
 
